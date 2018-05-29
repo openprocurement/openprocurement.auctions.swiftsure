80e8bf043c26619426872e961a27cb23c1787079
vitalii-motso <vitalii.motso@gmail.com>
2018-05-29 16:08:20 +0300

    add transferring test

---
+++ openprocurement/auctions/swiftsure/tests/transferring.py
@@ -0,0 +1,26 @@
+from openprocurement.auctions.swiftsure.tests.base import BaseAuctionWebTest
+from openprocurement.auctions.core.tests.plugins.transferring.mixins import AuctionOwnershipChangeTestCaseMixin
+
+class AuctionOwnershipChangeResourceTest(BaseAuctionWebTest,
+                                         AuctionOwnershipChangeTestCaseMixin):
+
+    first_owner = 'broker3'
+    second_owner = 'broker3'
+    test_owner = 'broker1t'
+    invalid_owner = 'broker1'
+    initial_auth = ('Basic', (first_owner, ''))
+
+    test_mode_test = None
+
+    def setUp(self):
+        super(AuctionOwnershipChangeResourceTest, self).setUp()
+        self.not_used_transfer = self.create_transfer()
+
+def suite():
+    tests = unittest.TestSuite()
+    tests.addTest(unittest.makeSuite(AuctionOwnershipChangeResourceTest))
+    return tests
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
